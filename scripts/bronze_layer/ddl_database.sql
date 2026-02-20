7fc150aed7d67e71ce08cbd794a7574f11ef5d70
Nikhitha Utada <128949572+nikhitha-utada@users.noreply.github.com>
2026-02-20 13:15:46 +0530

    description of layer
    Added steps and documentation

---
+++ scripts/bronze_layer/ddl_database.sql
@@ -0,0 +1,20 @@
+/*
+***************** This script is used to build the bronze layer***************
+================================================================================================
+ This layer loads the data as-is without any transformations on the data for debugging purposes
+ ================================================================================================
+STEPS INVOLVED:	
+===============
+	# create table structure using ddl from sources
+	# insert data from csv files into tables using bulk insert
+	# load the data into the bronze layer using stored procedures
+
+  ****************** CONCEPTS USED *******************
+	# DDL CREATE
+	# TRUNCATE (to truncate the table before creating it)
+	# BULK INSERT 
+	# DATETIME FUNCTIONS (to calculate the time taken to load the data)
+	# PRINT FUNCTION (to print messages on the console)
+	# STORED PROCEDURES (to load the data)
+	# TRY CATCH BLOCKS (to handle errors if thrown)
+/*
